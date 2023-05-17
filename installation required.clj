7db64320a5eae1848642eae440846ef338ba72d6
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-05-17 21:08:32 +0530

    Delete installation required.clj

--- installation required.clj
+++
@@ -1,9 +0,0 @@
-(defn installation_requirents[type_os]
-( (cond 
-     (= type_os "macOs")((exe_shell("brew install gh"))
-     (= type_os "Windows")((exe_shell("choco install gc"))
-                           )))
-    
-                     
-    
-                                     
