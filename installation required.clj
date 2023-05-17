5eddc0ca58866986f252034d5b0381fd0dcaebfd
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-05-17 06:08:09 +0530

    Update installation required.clj

--- installation required.clj
+++ installation required.clj
@@ -1,7 +1,7 @@
 (defn installation_requirents[type_os]
 ( (cond 
      (= type_os "macOs")((exe_shell("brew install gh"))
-     (= typeos " Windows")((exe_shell("choco install gc"))
+     (= type_os "Windows")((exe_shell("choco install gc"))
                            )))
     
                      
