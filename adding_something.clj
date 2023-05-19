d66035d7e34771eaffff14605df60161bc8ac056
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-05-19 23:35:11 +0530

    Update adding_something.clj

--- adding_something.clj
+++ adding_something.clj
@@ -2,6 +2,8 @@
  ;; This is a temporary work it get update soon
  ;; IDE runner app will out soon with large LLM with codes and implementations
  ;; Network connectivity all required to run this 
+ ;; Just ten-fifteen minutes of code added custom new features soon all things will out busy with doc so
+
 (def person "Having many products or codes or everything")
                          if (person)
                             (disp "Always ready to work for them help us this is single person work rather teams with loyalty") 
